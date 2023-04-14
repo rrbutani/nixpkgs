151d6d0a816dadbb6259a3e52ea7533094a7cede
Rahul Butani <rrbutani@users.noreply.github.com>
2023-04-14 18:23:47 -0500

    nevermind, I guess we need link.exe?

--- pkgs/tools/misc/lanzaboote/default.nix
+++ pkgs/tools/misc/lanzaboote/default.nix
@@ -1,4 +1,4 @@
-{ rustPlatform, fetchFromGitHub, lib }: rustPlatform.buildRustPackage rec {
+{ rustPlatform, stdenv, fetchFromGitHub, lib }: rustPlatform.buildRustPackage rec {
   pname = "lanzaboote-stub";
   version = "0.2.0";
   src = fetchFromGitHub {
@@ -17,6 +17,9 @@
   # TODO: limit supported platforms to UEFI
   meta.platforms = lib.platforms.all;
 
+
+  # `cc-wrapper`/our clang doesn't understand MSVC link opts (hack):
+  RUSTFLAGS = "-C linker=${stdenv.cc.targetPrefix}ld.lld -C linker-flavor=ld.lld";
 
   # Note: broken on i686-uefi: `flush_instruction_cache` is called but not
   # defined
