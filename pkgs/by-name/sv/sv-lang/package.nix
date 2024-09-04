b9cfcab66f9005f6b0bb5b690ccf8d26f8c6635f
Rahul Butani <rrbutani@users.noreply.github.com>
2024-09-04 05:36:56 -0700

    sv-lang: 3.0 → 6.0

--- pkgs/by-name/sv/sv-lang/package.nix
+++ pkgs/by-name/sv/sv-lang/package.nix
@@ -1,40 +1,29 @@
 { lib
 , stdenv
 , fetchFromGitHub
-, boost182
-, catch2_3
+, testers
+
+# build deps:
 , cmake
 , ninja
-, fmt_9
 , python3
-}:
 
-let
-  # dependency for this library has been removed in master (i.e. next release)
-  unordered_dense = stdenv.mkDerivation rec {
-    version = "2.0.1";
-    pname = "unordered_dense";
-    src = fetchFromGitHub {
-      owner = "martinus";
-      repo = pname;
-      rev = "v${version}";
-      sha256 = "sha256-9zlWYAY4lOQsL9+MYukqavBi5k96FvglRgznLIwwRyw=";
-    };
-    nativeBuildInputs = [
-      cmake
-    ];
-  };
+# runtime deps:
+, boost182
+, catch2_3
+, fmt
 
-in
-stdenv.mkDerivation rec {
+# options
+, doCheck ? true
+}: stdenv.mkDerivation (finalAttrs: {
   pname = "sv-lang";
-  version = "3.0";
+  version = "6.0";
 
   src = fetchFromGitHub {
     owner = "MikePopoloski";
     repo = "slang";
-    rev = "v${version}";
-    sha256 = "sha256-v2sStvukLFMRXGeATxvizmnwEPDE4kwnS06n+37OrJA=";
+    rev = "v${finalAttrs.version}";
+    sha256 = "sha256-mT8sfUz0H4jWM/SkV/uW4kmVKE9UQy6XieG65yJvIA8=";
   };
 
   cmakeFlags = [
@@ -42,27 +31,27 @@ stdenv.mkDerivation rec {
     "-DCMAKE_INSTALL_INCLUDEDIR=include"
     "-DCMAKE_INSTALL_LIBDIR=lib"
 
-    "-DSLANG_INCLUDE_TESTS=${if doCheck then "ON" else "OFF"}"
+    (lib.cmakeBool "SLANG_INCLUDE_TESTS" finalAttrs.doCheck)
+    (lib.cmakeBool "SLANG_USE_MIMALLOC" false)
   ];
 
   nativeBuildInputs = [
     cmake
-    python3
     ninja
-
-    # though only used in tests, cmake will complain its absence when configuring
-    catch2_3
+    python3
   ];
 
   buildInputs = [
-    unordered_dense
     boost182
-    fmt_9
+    fmt
   ];
 
-  # TODO: a mysterious linker error occurs when building the unittests on darwin.
-  # The error occurs when using catch2_3 in nixpkgs, not when fetching catch2_3 using CMake
-  doCheck = !stdenv.isDarwin;
+  nativeCheckInputs = [ catch2_3 ];
+  inherit doCheck;
+
+  passthru.tests = {
+    version = testers.testVersion { package = finalAttrs.finalPackage; };
+  };
 
   meta = with lib; {
     description = "SystemVerilog compiler and language services";
@@ -72,4 +61,4 @@ stdenv.mkDerivation rec {
     mainProgram = "slang";
     platforms = platforms.all;
   };
-}
+})
